ae54a2d70c42f177af23250526f119a2a7cead6c
Melissa Villalta <mevillaltas@hotmail.com>
2019-03-17 21:53:57 -0600

    LogIn

--- Segtransa_App/FrontEnd/frmLogin.Designer.cs
+++ Segtransa_App/FrontEnd/frmLogin.Designer.cs
@@ -41,68 +41,68 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.LightSkyBlue;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(6, 40);
+            this.label1.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(54, 43);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.Size = new System.Drawing.Size(120, 19);
             this.label1.TabIndex = 0;
             this.label1.Text = "Numero de Id:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.LightSkyBlue;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(50, 63);
+            this.label2.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(115, 81);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.Size = new System.Drawing.Size(59, 19);
             this.label2.TabIndex = 1;
             this.label2.Text = "Clave:";
             // 
             // tbxUserId
             // 
-            this.tbxUserId.Location = new System.Drawing.Point(97, 37);
-            this.tbxUserId.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbxUserId.Location = new System.Drawing.Point(189, 41);
+            this.tbxUserId.Margin = new System.Windows.Forms.Padding(2);
             this.tbxUserId.Name = "tbxUserId";
-            this.tbxUserId.Size = new System.Drawing.Size(114, 19);
+            this.tbxUserId.Size = new System.Drawing.Size(114, 21);
             this.tbxUserId.TabIndex = 2;
             // 
             // tbxPassword
             // 
-            this.tbxPassword.Location = new System.Drawing.Point(97, 63);
-            this.tbxPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbxPassword.Location = new System.Drawing.Point(189, 79);
+            this.tbxPassword.Margin = new System.Windows.Forms.Padding(2);
             this.tbxPassword.Name = "tbxPassword";
             this.tbxPassword.PasswordChar = '*';
-            this.tbxPassword.Size = new System.Drawing.Size(114, 19);
+            this.tbxPassword.Size = new System.Drawing.Size(114, 21);
             this.tbxPassword.TabIndex = 3;
             // 
             // groupBox1
             // 
-            this.groupBox1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.groupBox1.BackColor = System.Drawing.Color.LightGoldenrodYellow;
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.tbxPassword);
             this.groupBox1.Controls.Add(this.tbxUserId);
-            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(11, 55);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(116, 111);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupBox1.Size = new System.Drawing.Size(227, 98);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(462, 136);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Porfavor ingrese sus datos:";
             // 
             // btnIng
             // 
-            this.btnIng.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnIng.Location = new System.Drawing.Point(79, 177);
-            this.btnIng.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnIng.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIng.Location = new System.Drawing.Point(305, 261);
+            this.btnIng.Margin = new System.Windows.Forms.Padding(2);
             this.btnIng.Name = "btnIng";
-            this.btnIng.Size = new System.Drawing.Size(61, 24);
+            this.btnIng.Size = new System.Drawing.Size(118, 38);
             this.btnIng.TabIndex = 5;
             this.btnIng.Text = "Ingresar";
             this.btnIng.UseVisualStyleBackColor = true;
@@ -111,11 +111,11 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Stencil", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(10, 11);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(143, 26);
+            this.label3.Size = new System.Drawing.Size(119, 26);
             this.label3.TabIndex = 6;
             this.label3.Text = "Segtransa";
             // 
@@ -123,12 +123,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.SteelBlue;
-            this.ClientSize = new System.Drawing.Size(249, 211);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(746, 348);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.btnIng);
             this.Controls.Add(this.groupBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmLogin";
             this.Text = "Login";
             this.groupBox1.ResumeLayout(false);
